28f7d0094ec9170cbd04380d64a79343813337c0
EC2 Default User <ec2-user@ip-172-31-7-122.us-east-2.compute.internal>
2018-05-22 10:17:33 +0000

    kadai iroiro

--- app/Http/Controllers/TasklistController.php
+++ app/Http/Controllers/TasklistController.php
@@ -46,7 +46,13 @@ class TasklistController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'title' => 'required|max:10',
+            'content' => 'required|max:191',
+        ]);
+        
         $task = new Task;
+        $task->title = $request->title;
         $task->content = $request->content;
         $task->save();
         
@@ -92,10 +98,17 @@ class TasklistController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'title' => 'required|max:10',
+            'content' => 'required|max:191',
+        ]);
+        
         $task = Task::find($id);
+        $task->title = $request->title;
         $task->content = $request->content;
         $task->save();
-
+        
+        return redirect("/");
     }
 
     /**
